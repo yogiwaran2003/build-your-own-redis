aa6c1345bab3ac9a45402e81e1125614e893287c
S Yogeswaran <yogeshs9284@gmail.com>
2026-09-06 19:38:43 +0530

    RG2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +20,8 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+          OutputStream outputStream = clientSocket.getOutputStream();
+          outputStream.write("+PONG\r\n".getBytes());
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
